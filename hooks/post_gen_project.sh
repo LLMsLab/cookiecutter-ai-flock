784e7a25f41758c6088267070ef26c2adad0a29b
markeyser <marcosak@gmail.com>
2023-10-24 15:01:50 -0400

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,13 +7,10 @@ exec > >(tee -a script_output.log) 2>&1
 os_type="{{ cookiecutter.os_type }}"
 project_slug="{{ cookiecutter.project_slug }}"
 target_directory="{{ cookiecutter._output_dir }}"
-template_dir=template_dir="/c/Users/T93KQI0/.cookiecutters/cookiecutter-rag"  # You'll need to specify the path to your template directory
+template_dir="/c/Users/T93KQI0/.cookiecutters/cookiecutter-rag"  # Updated path
 
 # Construct the source filename
 source_filename="$template_dir/READMEs/README_${os_type,,}.md"
-
-# Construct the target filename
-target_filename="$target_directory/README.md"
 
 # Print the directories and filenames for debugging
 echo "Target Directory: $target_directory"
@@ -31,5 +28,6 @@ else
     echo "Source file $source_filename not found or not readable"
     exit 1
 fi
+
 
 
